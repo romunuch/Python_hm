54ee0eeb0cb9fc70f2f5096c452d55affa6bdcd7
RomanT <7889996@mail.ru>
2019-02-11 16:02:17 +0300

    fix 8_3

--- class8/8_3.py
+++ class8/8_3.py
@@ -1,9 +1,6 @@
-
-import time
-
 def input_search():
     return set(input('Find keys: ').split())
-start_time = time.time()
+
 def search(data):
     # result=[]
     with (open('/home/romunuch/univer/python/class8/StudentsDB.txt','r')) as DB:
@@ -13,10 +10,9 @@ def search(data):
         result=[student.split() for student in DB.readlines() if data<=set(student.split()) and data]
     return result
 
-result=search(set(['21']))
+result=search(input_search())
 if result:
     for i in result:
         print(f'Name:  {i[0]}\t\tSurname:  {i[1]}\t\tAge:  {i[2]}\t\tGender:  {i[3]}')
 else:
     print('No such students')        
-print("--- %s seconds ---" % (time.time() - start_time))
